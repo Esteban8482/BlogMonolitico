e9a46a1a9a4acc59a01ac2d3519351a35574d60b
Juan <juandiego.velez@utp.edu.co>
2025-09-23 12:05:03 -0500

    fix: error al no cargar credenciales firestore microservicio comment

--- microservices/comments-service/db_connector/firestore_client.py
+++ microservices/comments-service/db_connector/firestore_client.py
@@ -1,11 +1,14 @@
 from google.cloud import firestore
+from google.oauth2 import service_account
+import os
 
 _db = None
+
 
 def get_db():
     global _db
     if _db is None:
-        _db = firestore.Client()  
+        cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+        credentials = service_account.Credentials.from_service_account_file(cred_path)
+        _db = firestore.Client(credentials=credentials)
     return _db
-
-
